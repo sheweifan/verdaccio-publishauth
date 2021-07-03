d2b82144e6167a93463480cca5b073ab8a3a28f2
weifan she <923986234@qq.com>
2021-07-03 22:35:57 +0800

    add permission restriction plug-in

---
+++ index.js
@@ -0,0 +1,26 @@
+const commonsApi = require('@verdaccio/commons-api')
+
+class DynamicGroupPlugin {
+  constructor(config, stuff) {
+    this.config = config;
+  }
+
+  allow_publish(user, pkg, callback) {
+    const userName = user.name
+    const pkgVersion = pkg.version
+    const { users } = this.config
+
+    // console.log(`pkg`, pkg)
+    // console.log(`users`, users)
+    // console.log(`users.split(' ').indexOf(userName) > -1`, users.split(' ').indexOf(userName) > -1)
+    // console.log(`pkgVersion.indexOf('beta') > -1`, pkgVersion.indexOf('beta') > -1)
+
+    if (pkgVersion.indexOf('beta') > -1 ||  users.split(' ').indexOf(userName) > -1) {
+      return callback(null, true)
+    } else {
+      return callback(commonsApi.getUnauthorized('auth fail'))
+    }
+  }
+}
+
+module.exports = (cfg, stuff) => new DynamicGroupPlugin(cfg, stuff);
