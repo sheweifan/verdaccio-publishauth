4a7ab300b62c3b8fc14ae2d3ddc563f6e0678afe
weifan she <923986234@qq.com>
2021-07-05 19:11:46 +0800

    fix: error tips

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-const commonsApi = require('@verdaccio/commons-api')
-
+// const commonsApi = require('@verdaccio/commons-api')
+const httpError = require("http-errors");
 class DynamicGroupPlugin {
   constructor(config, stuff) {
     this.config = config;
   }
 
   allow_publish(user, pkg, callback) {
-    const userName = user.name
-    const pkgVersion = pkg.version
-    const { users } = this.config
+    const userName = user.name;
+    const pkgVersion = pkg.version;
+    const { users } = this.config;
 
     // console.log(`pkg`, pkg)
     // console.log(`users`, users)
     // console.log(`users.split(' ').indexOf(userName) > -1`, users.split(' ').indexOf(userName) > -1)
     // console.log(`pkgVersion.indexOf('beta') > -1`, pkgVersion.indexOf('beta') > -1)
 
-    if (pkgVersion.indexOf('beta') > -1 ||  users.split(' ').indexOf(userName) > -1) {
-      return callback(null, true)
+    if (
+      pkgVersion.indexOf("beta") > -1 ||
+      users.split(" ").indexOf(userName) > -1
+    ) {
+      return callback(null, true);
     } else {
-      return callback(commonsApi.getUnauthorized('You do not have permission to publish this version. Please contact the administrator.'))
+      return callback(
+        httpError[403](
+          "You do not have permission to publish this version. Please contact the administrator."
+        )
+      );
     }
   }
 }
